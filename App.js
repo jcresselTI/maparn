4f3d6a39bf68ae910b6e480beb67291379eb61c0
jcresselTI <87880806+jcresselTI@users.noreply.github.com>
2021-10-02 23:52:14 -0500

    atualiza

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React, {Component} from 'react';
+import { View, StyleSheet } from 'react-native';
+import { MapView } from 'react-native-maps';
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+    style={styles.map}
+    loadingEnabled={true}
+    region={{
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.015,
+    longitudeDelta: 0.0121,
+    }}
+   >
+   </MapView>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
+
   container: {
+
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+
   },
-});
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
+   },
+})
